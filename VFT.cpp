04c6a0c97e3e761395ab47fb619639ce90484d9f
lampese@github.com <1523284180@qq.com>
2022-02-04 00:44:30 +0800

    矢量图set.json生成功能合并

--- VFT.cpp
+++ VFT.cpp
@@ -7,48 +7,101 @@
 */
 #include<iostream>
 #include<fstream>
+#include<sstream>
 #include<cmath>
+#include "becbct.h"
+#include "jsonxx/json.hpp"
 #define roundt(x) round(x*100)/100.0
 using namespace std;
-double speed=1;
-inline double ctheta(double t,int m,double fai){
-	return t*m+fai;
+jsonxx::json sets={
+    {"Chains",3},
+    {"Define",{
+		{"1",{
+            {"Length",0},
+            {"Name","Fix"},
+            {"FirstCommandType","ncb"},
+            {"Define",{}}
+		}},
+        {"2",{
+            {"Length",0},
+            {"Name","Summon"},
+            {"FirstCommandType","ncb"},
+            {"Define",{}}
+		}},
+        {"3",{
+            {"Length",0},
+            {"Name","Series"},
+            {"FirstCommandType","rcb"},
+            {"Define",{}}
+		}}
+    }}
+};
+double speed=0.01;
+inline void init(){
+	ifstream filein("datas.txt");
+	int lines=0;
+	stringstream buffer;
+	buffer<<filein.rdbuf();
+	filein.close();
+	string process(buffer.str());
+	for(int i=0;i<process.size();++i)
+		if(process[i]=='['||process[i]==']')process.erase(i,1);
+		else if(process[i]==','){
+			++lines;
+			process[i]=' ';
+		}
+	ofstream fout("in.txt");
+	fout<<lines<<endl;
+	fout<<process;
+	fout.close();
 }
-inline long long gcd(long long x,long long y){
-    	while(y^=x^=y^=x%=y);
-	return x;
-}
-int main(){
+int main(int argc,char *argv[]){
+	setx=atoi(argv[1]),sety=atoi(argv[2]),setz=atoi(argv[3]),speed=atof(argv[4]);
+	init();
 	ifstream fin("in.txt");
 	int num,flag=1;
 	fin>>num;
 	int positive[num+1];
 	long long lthetas[num+1],gcdt;
-	double data1,data2,fai[num+1],alpha[num+1],omega[num+1],thetas[num+1];
-	for(int i=1;i<=num;i++){
+	TrigonometricFunction TFA[num+1];
+	double data1,data2;
+	for(int i=1;i<=num;++i){
 		fin>>data1>>data2;
-		alpha[i]=sqrt(data1*data1+data2*data2);
-		fai[i]=atan2(data2,data1);
+		TFA[i].alpha=sqrt(data1*data1+data2*data2);
+		TFA[i].fai=atan2(data2,data1);
 	}
 	fin.close();
-	for(int i=1;i<=num;i++){
-		double theta=ctheta(speed,((i+1)/2)*((i%2==0)?-1:1),fai[i]);
+	for(int i=1;i<=num;++i){
+		double theta=ctheta(speed,((i+1)/2)*((i%2==0)?-1:1),TFA[i].fai);
 		positive[i]=theta>0?1:theta<0?-1:0;
-		lthetas[i]=(long long)(fabs(theta-ctheta(2*speed,((i+1)/2)*((i%2==0)?-1:1),fai[i]))*1e8);
+		lthetas[i]=(long long)(fabs(theta-ctheta(2*speed,((i+1)/2)*((i%2==0)?-1:1),TFA[i].fai))*1e8);
 		gcdt=(i==1?abs(lthetas[i]):gcd(gcdt,abs(lthetas[i])));
 	}
-	for(int i=1;i<=num;i++)omega[i]=(lthetas[i]*1.0)/(gcdt*1.0)*(positive[i]*1.0);
+	for(int i=1;i<=num;++i)TFA[i].omgea=(lthetas[i]*1.0)/(gcdt*1.0)*(positive[i]*1.0);
 	while(flag){
 		flag=0;
-		for(int i=1;i<=num;i++)
-			if(omega[i]>10){
-				for(int i=1;i<=num;i++)omega[i]/=10.0;
+		for(int i=1;i<=num;++i)
+			if(TFA[i].omgea>10){
+				for(int i=1;i<=num;i++)TFA[i].omgea/=10.0;
 				flag=1;
 				break;
 			}
 	}
-	ofstream fout("set.txt");
-	fout<<num<<endl;
-	for(int i=1;i<=num;i++)fout<<roundt(omega[i])<<" "<<roundt(alpha[i])<<" "<<roundt(fai[i])<<endl;
+	TrigonometricFunction TF[num];
+	for(int i=1;i<=num;++i)TF[i-1]=move(TFA[i]);
+	auto Fix=InitialPhaseFix(TF,num);
+	auto Series=FourierSeriesBuilder(TF,num);
+	auto Summon=SummonCommandBuilder(num+1);
+	for(int i=0;i<Fix.size();++i)
+		sets["Define"]["1"]["Define"].push_back(Fix[i]);
+	sets["Define"]["1"]["Length"]=Fix.size();
+	for(int i=0;i<Summon.size();++i)
+		sets["Define"]["2"]["Define"].push_back(Summon[i]);
+	sets["Define"]["2"]["Length"]=Summon.size();
+	for(int i=0;i<Series.size();++i)
+		sets["Define"]["3"]["Define"].push_back(Series[i]);
+	sets["Define"]["3"]["Length"]=Series.size();
+	ofstream fout("set.json");
+	fout<<sets.dump();
 	fout.close();
 }
---
+++ becbct.h
@@ -0,0 +1,79 @@
+#pragma once
+#include<cmath>
+#include<cstdio>
+#include<string>
+#include<fstream>
+#include<iomanip>
+#include<iostream>
+#include<vector>
+#define roundt(x) round(x*100)/100.0
+using namespace std;
+struct TrigonometricFunction {
+	double alpha,omgea,fai;
+};
+int setx,sety,setz;
+inline double ctheta(double t,int m,double fai) {
+	return t*m+fai;
+}
+inline long long gcd(long long x,long long y) {
+	while(y^=x^=y^=x%=y);
+	return x;
+}
+string ArmorStandNameBuilder(int Num) {
+	int LowLow=Num%26, Low=(Num/26)%26, High=(Num/676)%26, HighHigh=Num/17576;
+	string Name;
+	if(HighHigh!=0) {
+		Name="";
+		Name+=char(HighHigh+65); 
+		Name+=char(High+65);
+		Name+=char(Low+65);
+		Name+=char(LowLow+65);
+	} else if(High!=0) {
+		Name="";
+		Name+=char(High+65);
+		Name+=char(Low+65);
+		Name+=char(LowLow+65);
+	} else if(Low!=0) {
+		Name="";
+		Name+=char(Low+65);
+		Name+=char(LowLow+65);
+	} else Name+=char(LowLow+65);
+	return Name;
+}
+vector<string> FourierSeriesBuilder(TrigonometricFunction TriFunc[],int n) {
+	string name,namep,SelectorA,SelectorB;
+	vector<string>commands;
+	for(register int i=0; i<n; ++i) {
+		name=ArmorStandNameBuilder(i);
+		namep=ArmorStandNameBuilder(i+1);
+		ostringstream ofa(ostringstream::ate);
+		ostringstream ofb(ostringstream::ate);
+		SelectorA="@e[type=armor_stand,name=\""+name+"\"]";
+		SelectorB="@e[type=armor_stand,name=\""+namep+"\"]";
+		ofa<<"execute "<<SelectorA<<" ~~~ tp @s ~~~ ~"<<roundt((-1)*TriFunc[i].omgea)<<"~";
+		ofb<<"execute "<<SelectorA<<" ~~~ tp "<<SelectorB<<" ^^^"<<roundt(TriFunc[i].alpha);
+		commands.push_back(ofa.str());
+		commands.push_back(ofb.str());
+	}
+	return commands;
+}
+vector<string> InitialPhaseFix(TrigonometricFunction TriFunc[],int n) {
+	string name,Selector;
+	vector<string>commands;
+	for(register int i=0; i<n; ++i) {
+		name=ArmorStandNameBuilder(i);
+		ostringstream ofs(ostringstream::ate);
+		Selector="@e[type=armor_stand,name=\""+name+"\"]";
+		commands.push_back("execute "+Selector+" ~~~ tp @s ~~~ 0 0");
+		TriFunc[i].fai-=round(TriFunc[i].fai/2/3.14159265358979)*2*3.14159265358979;
+		ofs<<"execute "<<Selector<<" ~~~ tp @s ~~~ ~"<<roundt(((-1)*TriFunc[i].fai*180/3.14159265358979))<<"~";
+		commands.push_back(ofs.str());
+	}
+	return commands;
+}
+vector<string> SummonCommandBuilder(int cb) {
+	vector<string>commands;
+	for(int i=1; i<=cb; i++)
+		commands.push_back("summon armor_stand "+ArmorStandNameBuilder(i-1)+" "+to_string(setx)+" "+to_string(sety)+" "+to_string(setz));
+	return commands;
+}
