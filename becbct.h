33f886836bd83a9e83d331250fdb0f6e3977bf24
KCN-judu <79190965+KCN-judu@users.noreply.github.com>
2022-10-06 20:04:49 +0800

    fixed a series of issues
    fixed omgea -> omega
    fixed fai -> phi
    fixed the issues of calculating phi
    removed define of roundt()

--- becbct.h
+++ becbct.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include<cmath>
 #include<cstdio>
 #include<string>
@@ -7,74 +8,81 @@
 #include<iostream>
 #include<vector>
 #include<sstream>
-#define roundt(x) round(x*100)/100.0
+
+#define pi 314.1592653589793238
 using namespace std;
 struct TrigonometricFunction {
-	double alpha,omgea,fai;
+    double alpha, omega, phi;
 };
-int setx,sety,setz;
-inline double ctheta(double t,int m,double fai) {
-	return t*m+fai;
+
+int setx, sety, setz;
+
+inline long long gcd(long long x, long long y) {
+    while (y ^= x ^= y ^= x %= y);
+    return x;
 }
-inline long long gcd(long long x,long long y) {
-	while(y^=x^=y^=x%=y);
-	return x;
-}
+
 string ArmorStandNameBuilder(int Num) {
-	int LowLow=Num%26, Low=(Num/26)%26, High=(Num/676)%26, HighHigh=Num/17576;
-	string Name;
-	if(HighHigh!=0) {
-		Name="";
-		Name+=char(HighHigh+65); 
-		Name+=char(High+65);
-		Name+=char(Low+65);
-		Name+=char(LowLow+65);
-	} else if(High!=0) {
-		Name="";
-		Name+=char(High+65);
-		Name+=char(Low+65);
-		Name+=char(LowLow+65);
-	} else if(Low!=0) {
-		Name="";
-		Name+=char(Low+65);
-		Name+=char(LowLow+65);
-	} else Name+=char(LowLow+65);
-	return Name;
+    int LowLow = Num % 26, Low = (Num / 26) % 26, High = (Num / 676) % 26, HighHigh = Num / 17576;
+    string Name;
+    if (HighHigh != 0) {
+        Name = "";
+        Name += char(HighHigh + 65);
+        Name += char(High + 65);
+        Name += char(Low + 65);
+        Name += char(LowLow + 65);
+    } else if (High != 0) {
+        Name = "";
+        Name += char(High + 65);
+        Name += char(Low + 65);
+        Name += char(LowLow + 65);
+    } else if (Low != 0) {
+        Name = "";
+        Name += char(Low + 65);
+        Name += char(LowLow + 65);
+    } else Name += char(LowLow + 65);
+    return Name;
 }
-vector<string> FourierSeriesBuilder(TrigonometricFunction TriFunc[],int n,double zoom) {
-	string name,namep,SelectorA,SelectorB;
-	vector<string>commands;
-	for(register int i=0; i<n; ++i) {
-		name=ArmorStandNameBuilder(i);
-		namep=ArmorStandNameBuilder(i+1);
-		ostringstream ofa(ostringstream::ate);
-		ostringstream ofb(ostringstream::ate);
-		SelectorA="@e[type=armor_stand,name=\""+name+"\"]";
-		SelectorB="@e[type=armor_stand,name=\""+namep+"\"]";
-		ofa<<"execute "<<SelectorA<<" ~~~ tp @s ~~~ ~"<<roundt((-1)*TriFunc[i].omgea)<<"~";
-		ofb<<"execute "<<SelectorA<<" ~~~ tp "<<SelectorB<<" ^^^"<<roundt(TriFunc[i].alpha*zoom);
-		commands.push_back(ofa.str());
-		commands.push_back(ofb.str());
-	}
-	return commands;
+
+vector <string> FourierSeriesBuilder(TrigonometricFunction TriFunc[], int n, double zoom) {
+    string name, name_p, SelectorA, SelectorB;
+    vector <string> commands;
+    for (int i = 0; i < n; ++i) {
+        name = ArmorStandNameBuilder(i);
+        name_p = ArmorStandNameBuilder(i + 1);
+        ostringstream ofa(ostringstream::ate);
+        ostringstream ofb(ostringstream::ate);
+        SelectorA = "@e[type=armor_stand,name=\"" + name + "\"]";
+        SelectorB = "@e[type=armor_stand,name=\"" + name_p + "\"]";
+        ofa << "execute " << SelectorA << " ~~~ tp @s ~~~ ~" << round((-100) * TriFunc[i].omega) / 100.0 << "~";
+        ofb << "execute " << SelectorA << " ~~~ tp " << SelectorB << " ^^^"
+            << round(100 * TriFunc[i].alpha * zoom) / 100.0;
+        commands.push_back(ofa.str());
+        commands.push_back(ofb.str());
+    }
+    return commands;
 }
-vector<string> InitialPhaseFix(TrigonometricFunction TriFunc[],int n) {
-	string name,Selector;
-	vector<string>commands;
-	for(register int i=0; i<n; ++i) {
-		name=ArmorStandNameBuilder(i);
-		ostringstream ofs(ostringstream::ate);
-		Selector="@e[type=armor_stand,name=\""+name+"\"]";
-		commands.push_back("execute "+Selector+" ~~~ tp @s ~~~ 0 0");
-		TriFunc[i].fai-=round(TriFunc[i].fai/2/3.14159265358979)*2*3.14159265358979;
-		ofs<<"execute "<<Selector<<" ~~~ tp @s ~~~ ~"<<roundt(((-1)*TriFunc[i].fai*180/3.14159265358979))<<"~";
-		commands.push_back(ofs.str());
-	}
-	return commands;
+
+vector <string> InitialPhaseFix(TrigonometricFunction TriFunc[], int n) {
+    string name, Selector;
+    vector <string> commands;
+    for (int i = 0; i < n; ++i) {
+        name = ArmorStandNameBuilder(i);
+        ostringstream ofs(ostringstream::ate);
+        Selector = "@e[type=armor_stand,name=\"" + name + "\"]";
+        commands.push_back("execute " + Selector + " ~~~ tp @s ~~~ 0 0");
+        ofs << "execute " << Selector << " ~~~ tp @s ~~~ ~"
+            << round(TriFunc[i].phi * (-pi) / 180.0) / 100.0 << "~";
+        commands.push_back(ofs.str());
+    }
+    return commands;
 }
-vector<string> SummonCommandBuilder(int cb) {
-	vector<string>commands;
-	for(int i=1; i<=cb; i++)
-		commands.push_back("summon armor_stand "+ArmorStandNameBuilder(i-1)+" "+to_string(setx)+" "+to_string(sety)+" "+to_string(setz));
-	return commands;
+
+vector <string> SummonCommandBuilder(int cb) {
+    vector <string> commands;
+    for (int i = 1; i <= cb; i++)
+        commands.push_back(
+                "summon armor_stand " + ArmorStandNameBuilder(i - 1) + " " + to_string(setx) + " " + to_string(sety) +
+                " " + to_string(setz));
+    return commands;
 }
